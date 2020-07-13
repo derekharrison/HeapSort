9151b3ec61e67cc15d222cf040e79ab3f753b42f
Derek <harrison.derek.w@gmail.com>
2020-07-13 17:35:12 +0200

    commit heapsort

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Apr 14, 2019
+ *      Author: d-w-h
+ *
+ *      Implementation of heap sort for arrays of ints
+ */
+
+#include <iostream>
+#include <cstdlib>
+#include "../inc/heap_sort.hpp"
+
+int main(int argc, char* argv[])
+{
+    int B[] = {1600, 400, 400, 400, 1500, 10, 14, 7, 9, 3, 2, 8, 1, 3, 5, 6, 11, 12, 13, 15};
+	int size = sizeof(B)/sizeof(int);
+	heapsort(B, size);
+    for(int i = 0; i < size; ++i) {
+    	std::cout << B[i] << std::endl;
+    }
+	return 0;
+}
